9371675d96f2fef254bd9d50221c48a72c6f6097
Cristhian <cristhian.web@gmail.com>
2023-01-06 11:57:32 -0500

    add eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,58 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true
+    env: {
+        browser: true,
+        node: true,
     },
-    "parser": "@typescript-eslint/parser",
-    "extends": [
-        "next/core-web-vitals",
-        "plugin:editorconfig/all",
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'next/core-web-vitals',
+        'prettier',
+        'plugin:editorconfig/all',
+        'plugin:editorconfig/noconflict',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
     ],
-    "plugins": [
-        "editorconfig",
-        "@typescript-eslint",
-        "simple-import-sort",
-        "import",
-        "unused-imports",
-        "folders"
+    plugins: [
+        '@typescript-eslint',
+        'editorconfig',
+        'simple-import-sort',
+        'import',
+        'unused-imports',
+        'folders',
     ],
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "import/first": "error",
-        "import/newline-after-import": "error",
-        "import/no-duplicates": "error",
-        "import/no-webpack-loader-syntax": "error",
-        "folders/match-regex": ["error", "^[a-z-]+$", "/js/"],
-        "simple-import-sort/exports": "error",
-        "simple-import-sort/imports": "error",
-        "unused-imports/no-unused-imports": "error",
-        "unused-imports/no-unused-vars": [
-            "warn",
+    rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+        'import/no-webpack-loader-syntax': 'error',
+        'folders/match-regex': ['error', '^[a-z-]+$', '/js/'],
+        'simple-import-sort/exports': 'error',
+        'simple-import-sort/imports': 'error',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
             {
-                "vars": "all",
-                "varsIgnorePattern": "^_",
-                "args": "after-used",
-                "argsIgnorePattern": "^_"
-            }
+                vars: 'all',
+                varsIgnorePattern: '^_',
+                args: 'after-used',
+                argsIgnorePattern: '^_',
+            },
         ],
-        "@typescript-eslint/no-non-null-assertion": "off"
+        '@typescript-eslint/no-non-null-assertion': 'off',
     },
-    "overrides": [
+    overrides: [
         {
-            "files": ["*.js"],
-            "rules": {
-                "@typescript-eslint/no-unsafe-assignment": "off",
-                "@typescript-eslint/no-unsafe-call": "off",
-                "@typescript-eslint/no-unsafe-member-access": "off",
-                "@typescript-eslint/no-var-requires": "off"
-            }
-        }
-    ]
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-unsafe-assignment': 'off',
+                '@typescript-eslint/no-unsafe-call': 'off',
+                '@typescript-eslint/no-unsafe-member-access': 'off',
+                '@typescript-eslint/no-var-requires': 'off',
+            },
+        },
+    ],
 };
--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,14 +1,14 @@
-import { Card, Grid, Row, Text } from "@nextui-org/react";
-import { useRouter } from "next/router";
+import {Card, Grid, Row, Text} from '@nextui-org/react';
+import {useRouter} from 'next/router';
 
-import { Pokemon } from "../../interfaces";
+import {Pokemon} from '../../interfaces';
 
 interface Props {
-    pokemon: Pokemon
+    pokemon: Pokemon;
 }
 
-export function PokemonCard({ pokemon }: Props) {
-    const { id, name, image } = pokemon;
+export function PokemonCard({pokemon}: Props) {
+    const {id, name, image} = pokemon;
     const router = useRouter();
 
     function onClick() {
@@ -18,8 +18,13 @@ export function PokemonCard({ pokemon }: Props) {
     return (
         <Grid xs={6} sm={3} md={2} xl={1}>
             <Card isHoverable isPressable onClick={onClick}>
-                <Card.Body css={{ p: 1 }}>
-                    <Card.Image src={image} width="100%" height={140} alt={name} />
+                <Card.Body css={{p: 1}}>
+                    <Card.Image
+                        src={image}
+                        width="100%"
+                        height={140}
+                        alt={name}
+                    />
                 </Card.Body>
                 <Card.Footer>
                     <Row justify="space-between">
@@ -31,4 +36,3 @@ export function PokemonCard({ pokemon }: Props) {
         </Grid>
     );
 }
-
