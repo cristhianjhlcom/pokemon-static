ad0367efa60fbd535b6e36595c8703012323a4fe
Cristhian <cristhian.web@gmail.com>
2023-01-06 16:32:49 -0500

    add new pokemon interface

--- interfaces/pokemonList.ts
+++ interfaces/pokemonList.ts
@@ -5,9 +5,22 @@ export interface PokemonResponse {
     results: Pokemon[];
 }
 
+interface PokemonSprites {
+    front_default: string;
+    back_default: string;
+    front_shiny: string;
+    back_shiny: string;
+    other: {
+        dream_world: {
+            front_default: string;
+        };
+    };
+}
+
 export interface Pokemon {
     id: string;
     name: string;
     url: string;
     image: string;
+    sprites: PokemonSprites;
 }
